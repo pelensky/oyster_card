8ac243b789b4e350ce5249bbcaa6142ae37742e6
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-15 11:53:46 +0000

    oystercard can be topped up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,4 +6,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(number)
+    @balance += number
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
   it "initializes with a balance of 0" do
     expect(oystercard.balance).to eq(0)
   end
+
+  it "can have its balance updated" do
+    oystercard.top_up(20)
+    expect(oystercard.balance).to eq 20
+  end
 end
