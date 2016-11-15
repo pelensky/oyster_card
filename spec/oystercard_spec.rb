499e092baec163db04e750e7033539d88a1173b7
Bryony Watson <bryonywatson1@googlemail.com>
2016-11-15 11:39:20 +0000

    step 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
+require './lib/oystercard'
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
+
+  it 'should have a balance' do
+    expect(oystercard).to respond_to (:balance)
+  end
+
+  it 'should be able to change the balance' do
+    oystercard = Oystercard.new(10)
+    expect(oystercard.balance).to eq 10
+  end
 
 end
