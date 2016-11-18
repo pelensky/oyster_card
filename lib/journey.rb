77d9bbd30296f38d3b414eff01a8e0d09d8560b6
Dan Pelensky <dan@pelensky.com>
2016-11-18 11:46:07 +0000

    fixed journey

---
+++ lib/journey.rb
@@ -0,0 +1,50 @@
+require './lib/oyster.rb'
+
+class Journey
+
+attr_reader :entry_station, :exit_station, :trip
+MINIMUM_FARE = 1
+
+#calculate the journey
+  def initialize(entry_station = nil, exit_station = nil)
+    @entry_station = entry_station
+    @exit_station = exit_station
+    @trip = {entry_station: nil, exit_station: nil}
+  end
+
+  def save_entry(station)
+    @entry_station = station
+    @trip[:entry_station] = station
+  end
+
+
+
+  def save_exit(station)
+    @exit_station = station
+  end
+
+  # def save_journey
+  #   @complete_journey[@entry_station] = @exit_station
+  # end
+
+
+def reset
+  delete_entry
+  delete_exit
+end
+
+def fare
+  MINIMUM_FARE
+end
+
+private
+
+  def delete_entry
+    @entry_station = nil
+  end
+
+  def delete_exit
+    @exit_station = nil
+  end
+
+end
